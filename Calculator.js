b84cf1fed0528d1473b3e3ebbd2190795de73442
Muhammad Subhan <muhammadsubhan54621@gmail.com>
2025-08-21 12:22:09 +0500

    Update Calculator.js

--- Calculator.js
+++ Calculator.js
@@ -1,1 +1,33 @@
-let calculation = '';
+      let calculation = localStorage.getItem('calculation') || '';
+
+      Output();
+
+      function updateCalculation(value) {
+        calculation += value;
+        localStorage.setItem('calculation', calculation);
+        Output();
+      }
+
+      function safeEval() {
+        try {
+          calculation = eval(calculation).toString(); 
+          localStorage.setItem('calculation', calculation);
+          Output();
+        } catch (e) {
+          alert('Invalid calculation!');
+          calculation = '';
+          Output();
+        }
+      }
+
+      function clearCalculation() {
+        calculation = '';
+        localStorage.removeItem('calculation');
+        Output();
+      }
+
+      function Output() {
+        document.querySelector('.js-Output')
+          .innerHTML = `Calculation: ${calculation}`;
+      }
+
